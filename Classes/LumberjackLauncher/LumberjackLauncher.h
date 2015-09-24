9395a92b61189197bc1dda925d2042e022387075
Yu Sugawara <dev@yusuga.com>
2015-09-24 18:30:41 +0900

    Tweak import header

--- Classes/LumberjackLauncher/LumberjackLauncher.h
+++ Classes/LumberjackLauncher/LumberjackLauncher.h
@@ -7,6 +7,7 @@
 
 #import <UIKit/UIKit.h>
 #import <CocoaLumberjack/CocoaLumberjack.h>
+#import "LumberjackLauncherMacros.h"
 
 /**
  ## Usage
--- Example/LumberjackLauncherExample/LumberjackLauncherExample/ViewController.m
+++ Example/LumberjackLauncherExample/LumberjackLauncherExample/ViewController.m
@@ -8,7 +8,6 @@
 
 #import "ViewController.h"
 #import "LumberjackLauncher.h"
-#import "LumberjackLauncherMacros.h"
 
 @interface ViewController ()
 
